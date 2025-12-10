3624698ac33fd9f8a71542ee43f0a030a61811d2
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-12-10 16:01:02 +0700

    fix: data todos

--- day3/todo-web/src/pages/dashboard/index.jsx
+++ day3/todo-web/src/pages/dashboard/index.jsx
@@ -48,14 +48,14 @@ export default function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            {todos.length === 0 ? (
+            {todos.data.length === 0 ? (
               <tr>
                 <td className="text-center p-4 text-gray-500">
                   No todos found
                 </td>
               </tr>
             ) : (
-              todos.map((item) => (
+              todos.data.map((item) => (
                 <tr key={item.id}>
                   <td className="border px-4 py-2">{item.id}</td>
                   <td className="border px-4 py-2">{item.title}</td>
