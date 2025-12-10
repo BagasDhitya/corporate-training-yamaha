a08cdb372571ca0f98b16fc8b1467e382b19a352
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-12-10 11:46:10 +0700

    feat: todo navbar

--- day3/todo-web/src/router/index.jsx
+++ day3/todo-web/src/router/index.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Example from "../pages/example";
 
+import TodoNavbar from "../components/TodoNavbar";
+
 export default function Router() {
   return (
     <BrowserRouter>
+      <TodoNavbar />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/example" element={<Example />} />
