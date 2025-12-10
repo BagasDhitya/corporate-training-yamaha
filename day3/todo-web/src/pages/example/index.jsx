994f0f6137582796bcea2eacf9701da3934abc2a
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-12-10 11:28:00 +0700

    feat: add responsivity example

--- day3/todo-web/src/pages/example/index.jsx
+++ day3/todo-web/src/pages/example/index.jsx
@@ -1,3 +1,15 @@
 export default function Example() {
-  return <div>Example</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1>Tailwind Responsivity</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-6 md:grid-cols-2 sm:grid-cols-1">
+        <div className="p-5 bg-blue-500 rounded-md text-white">Item 1</div>
+        <div className="p-5 bg-blue-500 rounded-md text-white">Item 2</div>
+        <div className="p-5 bg-blue-500 rounded-md text-white">Item 3</div>
+        <div className="p-5 bg-blue-500 rounded-md text-white">Item 4</div>
+        <div className="p-5 bg-blue-500 rounded-md text-white">Item 5</div>
+        <div className="p-5 bg-blue-500 rounded-md text-white">Item 6</div>
+      </div>
+    </div>
+  );
 }
