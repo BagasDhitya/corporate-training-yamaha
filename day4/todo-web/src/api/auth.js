1c66f1b9ae9180899b5e60da1550b19374850962
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-12-11 16:01:24 +0700

    feat: handle auth api

---
+++ day4/todo-web/src/api/auth.js
@@ -0,0 +1,21 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export async function registerUser(payload) {
+    const response = await fetch(`${BASE_URL}/auth/register`, {
+        method: 'POST',
+        headers: 'Content-Type : application/json',
+        body: JSON.stringify(payload)
+    })
+    const result = await response.json()
+    return result
+}
+
+export async function loginUser(payload) {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+        method: 'POST',
+        headers: 'Content-Type : application/json',
+        body: JSON.stringify(payload)
+    })
+    const result = await response.json()
+    return result
+}
